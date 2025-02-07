845d6ed67f465f242752ebf6ab9605c7e261339c
Gowtham <your.email@example.com>
2025-02-07 22:04:22 +0530

    Feat:Serving HTML

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {createServer} from 'node:http'
+import {fileURLToPath} from 'node:url'
+import { dirname,join } from 'node:path'
+
+const app=express()
+const server=createServer(app)
+
+const __dirname=dirname(fileURLToPath(import.meta.url))
+
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'index.html'))
+})
+server.listen(3000,()=>{
+    console.log('serevr running aat http://localhost:3000')
+})
